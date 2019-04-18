1d49631c96c46e1fe0566c79cad3e45b33f9adb6
Sergey Sukharevich <mr.boss2142bf@gmail.com>
2019-04-18 10:46:10 +0300

    final commit

---
+++ Form2/Form2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Form2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonAscending = new System.Windows.Forms.Button();
+            this.buttonDescending = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonMax = new System.Windows.Forms.Button();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.button_first = new System.Windows.Forms.Button();
+            this.button_last = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(26, 52);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 46);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(30, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размер коллекции";
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.BackColor = System.Drawing.Color.Silver;
+            this.buttonGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGenerate.ForeColor = System.Drawing.Color.DarkBlue;
+            this.buttonGenerate.Location = new System.Drawing.Point(26, 122);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(305, 73);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "Генерация";
+            this.buttonGenerate.UseVisualStyleBackColor = false;
+            this.buttonGenerate.Click += new System.EventHandler(this.Generation);
+            // 
+            // buttonAscending
+            // 
+            this.buttonAscending.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAscending.Location = new System.Drawing.Point(250, 356);
+            this.buttonAscending.Name = "buttonAscending";
+            this.buttonAscending.Size = new System.Drawing.Size(175, 55);
+            this.buttonAscending.TabIndex = 3;
+            this.buttonAscending.Text = "Сортировать по возрастанию";
+            this.buttonAscending.UseVisualStyleBackColor = true;
+            this.buttonAscending.Click += new System.EventHandler(this.buttonInAscendingOrder);
+            // 
+            // buttonDescending
+            // 
+            this.buttonDescending.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDescending.Location = new System.Drawing.Point(16, 354);
+            this.buttonDescending.Name = "buttonDescending";
+            this.buttonDescending.Size = new System.Drawing.Size(175, 57);
+            this.buttonDescending.TabIndex = 4;
+            this.buttonDescending.Text = "Сортировать по убыванию";
+            this.buttonDescending.UseVisualStyleBackColor = true;
+            this.buttonDescending.Click += new System.EventHandler(this.buttonInDescendingOrder);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(753, 33);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(251, 522);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(1027, 33);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(251, 522);
+            this.textBox3.TabIndex = 6;
+            // 
+            // buttonMax
+            // 
+            this.buttonMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMax.Location = new System.Drawing.Point(16, 428);
+            this.buttonMax.Name = "buttonMax";
+            this.buttonMax.Size = new System.Drawing.Size(175, 57);
+            this.buttonMax.TabIndex = 7;
+            this.buttonMax.Text = "Максимальный элемент";
+            this.buttonMax.UseVisualStyleBackColor = true;
+            this.buttonMax.Click += new System.EventHandler(this.buttonMaxClick);
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMin.Location = new System.Drawing.Point(250, 428);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(175, 57);
+            this.buttonMin.TabIndex = 8;
+            this.buttonMin.Text = "Минимальный элемент";
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMinClick);
+            // 
+            // button_first
+            // 
+            this.button_first.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_first.Location = new System.Drawing.Point(16, 501);
+            this.button_first.Name = "button_first";
+            this.button_first.Size = new System.Drawing.Size(175, 57);
+            this.button_first.TabIndex = 9;
+            this.button_first.Text = "Первый элемент";
+            this.button_first.UseVisualStyleBackColor = true;
+            this.button_first.Click += new System.EventHandler(this.buttonFirstClick);
+            // 
+            // button_last
+            // 
+            this.button_last.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_last.Location = new System.Drawing.Point(250, 501);
+            this.button_last.Name = "button_last";
+            this.button_last.Size = new System.Drawing.Size(175, 57);
+            this.button_last.TabIndex = 10;
+            this.button_last.Text = "Последний элемент";
+            this.button_last.UseVisualStyleBackColor = true;
+            this.button_last.Click += new System.EventHandler(this.buttonLastClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1319, 601);
+            this.Controls.Add(this.button_last);
+            this.Controls.Add(this.button_first);
+            this.Controls.Add(this.buttonMin);
+            this.Controls.Add(this.buttonMax);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonDescending);
+            this.Controls.Add(this.buttonAscending);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonAscending;
+        private System.Windows.Forms.Button buttonDescending;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonMax;
+        private System.Windows.Forms.Button buttonMin;
+        private System.Windows.Forms.Button button_first;
+        private System.Windows.Forms.Button button_last;
+    }
+}
+
---
+++ Form2/Form2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> list = new List<int>();
+        Random rnd = new Random();
+        private void Generation(object sender, EventArgs e)
+        {
+            try 
+            {
+                textBox2.Clear();
+                textBox3.Clear();
+                list.Clear();
+                for (int i = 0; i < int.Parse(textBox1.Text); i++)
+                {
+                    list.Add(rnd.Next(10,500));
+                    textBox2.Text += "Человек №  " + list[i] + "\r\n";
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+
+        private void buttonInAscendingOrder(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            var z = list.OrderBy(s => s);
+            foreach (var elem in z)
+            {
+                textBox3.Text += "Человек №  " + elem + "\r\n";
+            }
+        }
+
+        private void buttonInDescendingOrder(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            var z = list.OrderByDescending(s => s);
+            foreach (var elem in z)
+            {
+                textBox3.Text += "Человек №  " + elem + "\r\n";
+            }
+        }
+
+        private void buttonMaxClick(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox3.Text += "Человек №  " + list.Max() + "\r\n";
+        }
+
+        private void buttonMinClick(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox3.Text += "Человек №  " + list.Min() + "\r\n";
+        }
+
+        private void buttonFirstClick(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox3.Text += "Человек №  " + list.First() + "\r\n";
+        }
+
+        private void buttonLastClick(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox3.Text += "Человек №  " + list.Last() + "\r\n";
+        }
+    }
+}
